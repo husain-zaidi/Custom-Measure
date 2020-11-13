8edf174445efb6f3a19cd747fdea48953b561b2c
Husain Zaidi <husainhz7@gmail.com>
2020-11-13 14:19:10 +0000

    added vue-router

---
+++ Router.vue
@@ -0,0 +1,11 @@
+<template>
+    <router-view></router-view>
+</template>
+
+<script>
+
+import VueRouter from 'vue-router'
+export default {
+    name: "routerRender"
+}
+</script>
--- index.js
+++ index.js
@@ -3,19 +3,35 @@ import Vue from "vue";
 import "vuetify/dist/vuetify.min.css";
 import Vuetify from "vuetify";
 import app from './App'
+import VueRouter from 'vue-router'
+import routerRender from'./Router'
 
+const routes = [
+    { path: '/custom-measure', component: app },
+    { path: '/', component: app }
+  ]
+  
+const router = new VueRouter({
+    routes // short for `routes: routes`
+})
+  
 
 window.onload = (e) => {
 
     Vue.use(Vuetify);
+    Vue.use(VueRouter);
     new Vue({
         el: '#app',
+        router: router,
         render: h => h(app),
         vuetify: new Vuetify( {
             theme: {
             },
         }),
     })
-
+    // const app = new Vue({
+    //     router,
+    //     render: h => h(routerRender)
+    // }).$mount('#app');
 
 }
